37f6cf2d827878442a410f10b79781c66285453a
dongfanai <dongfanai@krspace.cn>
2017-06-23 11:32:33 +0800

    doc:

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 ## 基于webpack搭建一个react项目    
 
-#### webpack 基础    
+### webpack 基础    
 
- ##### 安装webpack     
+ ### 安装webpack     
  
  作为全局安装 
 
- ```
- 	$ npm install webpack -g
- 	$ webpack -v    
- ```
-
-
+```
+$ npm install webpack -g
+$ webpack -v    
+```
+作为项目依赖安装 
+  
+```
+$ npm install --save webpack      
+```
 
 
 
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,16 +1,37 @@
 var path=require('path');
 
-
-
 var config={
-	entry:'./src/main.js',
+	entry:[
+		path:path.resolve(__dirname,'./dist'),
+		filename:'bundle.js',//'./src/main.js',
+	],
 	output:{
 		path:path.resolve(__dirname,'./dist'),
 		filename:'bundle.js'
-	}
-
-
-
+	},
+	module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    },
+    resolve: {
+		extensions: ['', '.js', '.less', '.png', '.jpg', '.svg'],
+		alias: {
+			'kr-ui': path.join(process.cwd(), '/src/Components'),
+			'kr': path.join(process.cwd(), '/src'),
+			'redux': path.join(node_modules_dir, 'redux'),
+			'react-redux': path.join(node_modules_dir, 'react-redux'),
+			'mobx': path.join(node_modules_dir, 'mobx'),
+			'mobx-react': path.join(node_modules_dir, 'mobx-react'),
+			'react-router': path.join(node_modules_dir, 'react-router'),
+			'material-ui': path.join(node_modules_dir, 'material-ui'),
+			'lodash': path.join(node_modules_dir, 'lodash')
+		},
+	},
+	plugins: [
+	    new webpack.optimize.UglifyJsPlugin(),
+	    new HtmlWebpackPlugin({template: './src/index.html'})
+	]
 }
 
 module.exports=config;
